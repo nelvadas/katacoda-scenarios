b60477fa6c6950a446c13b67bf712f725b9320dc
nelvadas <nelvadas@gmail.com>
2020-04-29 17:45:11 +0200

    Java Home

--- graalvm/graalvm-compiler/assets/app.js
+++ graalvm/graalvm-compiler/assets/app.js
@@ -4,6 +4,7 @@ export GRAAL_VERSION=20.0.0
 curl -L -o /tmp/graalvm-ce.tar.gz https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${GRAAL_VERSION}/graalvm-ce-java${JDK_VERSION}-linux-amd64-${GRAAL_VERSION}.tar.gz
 tar -C /opt/ -xvf /tmp/graalvm-ce.tar.gz
 export PATH=/opt/graalvm-ce-java${JDK_VERSION}-${GRAAL_VERSION}/bin:$PATH
+export JAVA_HOME=/opt/graalvm-ce-java${JDK_VERSION}-${GRAAL_VERSION}
 
 apt update
 apt-get install -y maven
