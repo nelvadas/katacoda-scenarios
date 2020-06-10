55da30acfe8525dc458d7ba2283151c510d7261b
nelvadas <nelvadas@gmail.com>
2020-06-10 17:43:43 +0200

    Get GraalVM

--- graalvm/graalvm-polyglot/intro-course-data-fg.sh
+++ graalvm/graalvm-polyglot/intro-course-data-fg.sh
@@ -2,6 +2,6 @@
 #! /bin/bash/sh
 
 #This script is meant for quick & easy install via 
-curl -fsSL https://raw.githubusercontent.com/nelvadas/katacoda-scenarios/master/graalvm/scripts/get-graalvm.sh
+curl -fsSL https://raw.githubusercontent.com/nelvadas/katacoda-scenarios/master/graalvm/scripts/get-graalvm.sh -o  get-graalvm.sh
 sh get-graalvm.sh
 
